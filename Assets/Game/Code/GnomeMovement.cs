c5217ad26fec9de8488c53c34c09224639d2d377
Debbie Eilers <debbieeilers@web.de>
2017-03-10 17:24:13 +0100

    Enemys navigation, spawning

---
+++ Assets/Game/Code/GnomeMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GnomeMovement : MonoBehaviour {
+
+    Transform player;
+    NavMeshAgent nav;
+
+
+    // Use this for initialization
+    void Awake () {
+        player = GameObject.FindWithTag("Player").transform;
+        nav = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        nav.SetDestination(player.position);
+	}
+}
